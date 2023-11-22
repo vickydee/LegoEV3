44315bf63931e99da6e5346251808ab9e2e31503
vickydee <79496219+vickydee@users.noreply.github.com>
2023-11-22 13:21:28 -0700

    Add files via upload

--- main.m
+++ main.m
@@ -1,3 +1,4 @@
+stoppedBlue = 0;
 
 while 1
 %% Initializing variables in  loop to constantly update the values
@@ -8,7 +9,38 @@ while 1
     touch1 = brick.TouchPressed(3); 
 
   % color
-    run('colorSensor.m')
+    brick.SetColorMode(1, 2);
+    color = brick.ColorCode(1);
+
+ %% red
+     if(color == 5)
+       run('stop.m');
+       pause(1);
+       run('moveForward.m');
+       pause(1.7);
+     end
+
+%% blue
+    if(color == 2)
+        stoppedBlue = 1;
+        disp(stoppedBlue)
+        disp("blue");
+        run('stop.m');
+        run('remotecontrol.m');
+    end
+%% green
+   if (color == 3)
+       disp("green");
+       if  (stoppedBlue == 1)
+        run('stop.m');
+        run('remotecontrol.m');
+       end
+    end
+%% yellow
+    if (color == 7)
+        brick.beep;
+    end
+
 
 % Moves forward
     run('moveForward.m')
